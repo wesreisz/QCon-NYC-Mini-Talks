a3821afbd6344bdebed2998f435705bb8e5e797c
Wesley Reisz <wes@wesleyreisz.com>
2015-06-03 09:26:45 -0400

    added intro slide deck from morning talk and added numbers slides to match morning deck

--- Machine Intelligence, Algorithms for Anti-Money Laundering, & Blockchain/build.py
+++ Machine Intelligence, Algorithms for Anti-Money Laundering, & Blockchain/build.py
@@ -1,11 +1,13 @@
 import os
 from PyPDF2 import PdfFileReader, PdfFileMerger
 
+merged_file_name="merged_full.pdf"
 files_dir = "./" 
-os.remove("merged_full.pdf")
+if os.path.exists(merged_file_name):
+	os.remove(merged_file_name)
 pdf_files = [f for f in os.listdir(files_dir) if f.endswith("pdf")]
 merger = PdfFileMerger()
 for filename in pdf_files:
     merger.append(PdfFileReader(os.path.join(files_dir, filename), "rb"))
 
-merger.write(os.path.join(files_dir, "merged_full.pdf"))
+merger.write(os.path.join(files_dir, merged_file_name))
